c4205f3299fa8612d0256e16b32cc189243d3112
Pavit <pavitpim@gmail.com>
2022-06-23 01:00:10 +0700

    ADD:header and banner

---
+++ components/Banner.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Banner() {
+  return (
+    <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
+      <div className="px-10 space-y-5">
+        <h1 className="text-6xl font-serif">
+          {" "}
+          <span className="underline decoration-black decoration-4">
+            Medium
+          </span>{" "}
+          is a place to write, read, and connect
+        </h1>
+        <h2>
+          It's easy and free to post your thinking on any topic and connect with
+          millions of readers.
+        </h2>
+      </div>
+      <img
+        className="hidden h-32 md:inline-flex lg:h-full"
+        src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default Banner;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Banner from '../components/Banner'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-     <h1>This is Medium 2.0</h1>
+     <Header />
+     <Banner />
+   
     </div>
   )
 }
